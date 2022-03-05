00aa7820282418af83f0da4bc251eb57d45fbb9a
JM09 <jassimbackup62@gmail.com>
2022-03-05 13:31:26 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var hobby = "النوم"
+
+let name = "جاسم"
+
+var favouriteNumber = 9
+
+print("السلام عليكم معاكم \(name) و رقمي المفضل هو \(favouriteNumber) و احب \(hobby)")
+
